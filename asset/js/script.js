8885f48de7918f5d602c1acb64078dffa31c67a1
Kazi Robin <kazirobinking@gmail.com>
2023-10-30 10:46:21 +0600

    update(30/10/23)

--- asset/js/script.js
+++ asset/js/script.js
@@ -4,3 +4,18 @@ const navbarBar =document.querySelector('.navbar_menu');
 navbar.addEventListener('click',() => {
     navbarBar.classList.toggle('active')
 })
+// slider section 
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(direction) {
+    slides[currentSlide].classList.remove('active-slide');
+
+    if (direction === 'prev') {
+        currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    } else if (direction === 'next') {
+        currentSlide = (currentSlide + 1) % slides.length;
+    }
+
+    slides[currentSlide].classList.add('active-slide');
+}
