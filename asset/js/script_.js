c92df565e8ef9dda888263faa3bc1c08912289d2
Kazi Robin <kazirobinking@gmail.com>
2023-11-11 07:11:00 +0600

    up

--- asset/js/script_.js
+++ asset/js/script_.js
@@ -1,7 +1,6 @@
 // navbar link hover effect
 const activeLink = document.querySelectorAll('.navbar_menu_item_link');
 
-
 for(let i=1;i<activeLink.length;i++){
     activeLink[i].addEventListener('mouseover',() => {activeLink[i].classList.add('active')})
     activeLink[i].addEventListener('mouseout',() => {activeLink[i].classList.remove('active')})
